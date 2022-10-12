f6b3b734f1066b9ff33354f8c3526ece63b70b41
Jay <jay.smith.soundengineer@gmail.com>
2022-10-12 16:10:38 +0100

    Initial Commit

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from h11 import Data
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+##WTF Form for COMMENTS
+class CommentForm(FlaskForm):
+    body = CKEditorField("Post A Comment", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
+
+
+##Create WTForm for registering
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("SIGN ME UP!")
+
+
+##Create WTForm for logging in
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
