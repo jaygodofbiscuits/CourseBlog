2791405bbddad98548791902a79d8366916c05a3
Jay <jay.smith.soundengineer@gmail.com>
2022-10-14 14:04:18 +0100

    Fix typo in forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import FlaskForm
-from h11 import Data
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
